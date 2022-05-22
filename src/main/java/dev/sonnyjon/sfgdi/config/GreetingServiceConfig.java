05026ea55819ba24531d2c612f2aab8f18b4e4f0
sonnyjon <sonny.s.jon@gmail.com>
2022-05-22 14:49:49 -0700

    Removed annotations from service classes and moved the annotations to the configuration class

--- src/main/java/dev/sonnyjon/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/dev/sonnyjon/sfgdi/config/GreetingServiceConfig.java
@@ -1,16 +1,34 @@
 package dev.sonnyjon.sfgdi.config;
 
-import dev.sonnyjon.sfgdi.services.ConstructorGreetingService;
-import dev.sonnyjon.sfgdi.services.PropertyInjectedGreetingService;
-import dev.sonnyjon.sfgdi.services.SetterInjectedGreetingService;
+import dev.sonnyjon.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 /**
  * Created by Sonny on 5/22/2022.
  */
 @Configuration
 public class GreetingServiceConfig {
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService() {
+        return new I18nEnglishGreetingService();
+    }
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18nSpanishGreetingService i18nSpanishGreetingService() {
+        return new I18nSpanishGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService() {
